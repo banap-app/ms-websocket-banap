35becafb74951f12de4c61aba87983e918c5471e
Victor Luís <victorluiscaguiar@gmail.com>
2025-04-10 19:09:02 -0300

    feat: add UserID value object

---
+++ src/domain/UserID.ts
@@ -0,0 +1,17 @@
+import { validate as uuidValidate } from "uuid";
+
+export class UserID {
+    private constructor(private readonly value: string) {
+        if (!uuidValidate(value)) {
+            throw new Error("Invalid UUID for UserID");
+        }
+    }
+
+    public static from(value: string): UserID {
+        return new UserID(value);
+    }
+
+    public toString(): string {
+        return this.value;
+    }
+}
--- src/domain/entities/Message.ts
+++ src/domain/entities/Message.ts
@@ -1,19 +1,20 @@
 import { MessageID } from "../MessageID";
 import { MessageType } from "../MessageType";
+import { UserID } from "../UserID";
 
 export class Message {
     constructor(
         public readonly id: MessageID,
-        public readonly senderId: string | null, // null for system messages
-        public readonly recipientId: string,
+        public readonly senderId: UserID | null, // null for system messages
+        public readonly recipientId: UserID,
         public readonly type: MessageType, // e.g., 'notification', 'chat', 'system'
         public readonly content: string,
         public readonly timestamp: Date,
     ) {}
 
     static create(
-        senderId: string,
-        recipientId: string,
+        senderId: UserID,
+        recipientId: UserID,
         type: MessageType,
         content: string,
         timestamp: Date,
