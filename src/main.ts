c340296301dd20af4b8f506e08842a263513195b
Victor Luís <victorluiscaguiar@gmail.com>
2025-04-11 18:43:12 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
 
+app.use(cors());
 app.use(express.json());
 
 io.on("connection", (socket) => {
